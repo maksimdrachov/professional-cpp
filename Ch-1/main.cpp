2a07e6ae62e5f87ea19ecda07af967c372e3b96f
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-19 19:43:49 +0200

    Ch1

---
+++ Ch-1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "employee.hpp"
+
+int main() {
+    // Records::Employee first_employee{"Maksim", "Drachov"};
+    #ifdef __cpp_lib_format
+    std::cout << "std::format is supported" << std::endl;
+    #else
+    std::cout << "std::format is not supported" << std::endl;
+    #endif
+
+    return 0;
+}
