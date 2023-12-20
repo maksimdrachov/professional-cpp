1b2431021db1ad15713b29161837b53fe22536d9
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-20 15:56:01 +0200

    19_AirlineTicket

---
+++ Ch-1/19_AirlineTicket/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <format>
+#include "AirlineTicket.hpp"
+
+using namespace std;
+
+int main()
+{
+	AirlineTicket myTicket;  // Stack-based AirlineTicket
+	myTicket.setPassengerName("Sherman T. Socketwrench");
+	myTicket.setNumberOfMiles(700);
+	double cost{ myTicket.calculatePriceInDollars() };
+	cout << format("This ticket will cost ${}", cost) << endl;
+}
