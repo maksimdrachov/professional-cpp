91c9e394b49ccfeb152bbce57493b1d90c5c58cf
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-20 15:52:54 +0200

    18_StringTest

---
+++ Ch-1/18_StringTest/string.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	string myString{ "Hello, World" };
+	cout << format("The value of myString is {}", myString) << endl;
+	cout << format("The second letter is {}", myString[1]) << endl;
+}
