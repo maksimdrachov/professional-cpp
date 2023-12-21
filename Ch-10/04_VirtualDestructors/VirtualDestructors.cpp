29688f3eff3e7246456f8d1ba6ab3f50e3aaf148
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-21 18:23:35 +0200

    04_VirtualDestructors

---
+++ Ch-10/04_VirtualDestructors/VirtualDestructors.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+	Base() {}
+	~Base() {} // This should be virtual!!
+};
+
+class Derived : public Base
+{
+public:
+	Derived()
+	{
+		m_string = new char[30];
+		cout << "m_string allocated" << endl;
+	}
+
+	~Derived()
+	{
+		delete[] m_string;
+		cout << "m_string deallocated" << endl;
+	}
+private:
+	char* m_string;
+};
+
+int main()
+{
+	Base* ptr{ new Derived{} };   // m_string is allocated here.
+	delete ptr; // ~Base is called, but not ~Derived because the destructor
+				// is not virtual!
+}
