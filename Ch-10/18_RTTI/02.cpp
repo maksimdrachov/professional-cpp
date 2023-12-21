cfc31c3c7b1cebe18e10f1a6e755d7e63fe50960
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-21 19:09:09 +0200

    18_RTTI

---
+++ Ch-10/18_RTTI/02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+using namespace std;
+
+class Loggable
+{
+public:
+	virtual ~Loggable() = default;
+	virtual std::string getLogMessage() const = 0;
+};
+
+class Foo : public Loggable
+{
+public:
+	std::string getLogMessage() const override { return "Hello logger."; }
+};
+
+void logObject(const Loggable& loggableObject)
+{
+	cout << typeid(loggableObject).name() << ": ";
+	cout << loggableObject.getLogMessage() << endl;
+}
+
+int main()
+{
+	Foo f;
+	logObject(f);
+}
