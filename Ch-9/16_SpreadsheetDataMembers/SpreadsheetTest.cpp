642602c0c631809f605697fc00ac99eeb10531d4
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-21 14:31:56 +0200

    16_SpreadsheetDataMembers

---
+++ Ch-9/16_SpreadsheetDataMembers/SpreadsheetTest.cpp
@@ -0,0 +1,13 @@
+#include "Spreadsheet.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	SpreadsheetApplication app;
+	Spreadsheet s1 { 2, 3, app };
+	Spreadsheet s2 { 3, 4, app };
+
+	cout << "Maximum height is: " << Spreadsheet::MaxHeight << endl;
+}
