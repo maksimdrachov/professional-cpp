963b9dd7858b97b61ad0893deaf4684d8c6a2241
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-21 12:29:15 +0200

    02_SpreadsheetNoCopyAssign

---
+++ Ch-9/02_SpreadsheetNoCopyAssign/Spreadsheet.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstddef>
+
+// export module spreadsheet;
+
+#include "SpreadsheetCell.hpp"
+
+class Spreadsheet
+{
+public:
+	Spreadsheet(size_t width, size_t height);
+	Spreadsheet(const Spreadsheet& src) = delete;
+	~Spreadsheet();
+
+	Spreadsheet& operator=(const Spreadsheet& rhs) = delete;
+
+	void setCellAt(size_t x, size_t y, const SpreadsheetCell& cell);
+	SpreadsheetCell& getCellAt(size_t x, size_t y);
+
+	void swap(Spreadsheet& other) noexcept;
+
+private:
+	void verifyCoordinate(size_t x, size_t y) const;
+
+	size_t m_width{ 0 };
+	size_t m_height{ 0 };
+	SpreadsheetCell** m_cells{ nullptr };
+};
+
+void swap(Spreadsheet& first, Spreadsheet& second) noexcept;
