c3b7c28adbfbca95d374bf2e85dc5e6512d7276c
Maksim Drachov <maksim.drachov@zubax.com>
2023-12-21 12:23:40 +0200

    01_Spreadsheet

---
+++ Ch-9/01_Spreadsheet/Spreadsheet.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstddef>
+
+#include "Spreadsheet.hpp"
+
+#include "SpreadsheetCell.hpp"
+
+class Spreadsheet
+{
+public:
+	Spreadsheet(size_t width, size_t height);
+	Spreadsheet(const Spreadsheet& src);
+	~Spreadsheet();
+
+	Spreadsheet& operator=(const Spreadsheet& rhs);
+
+	void setCellAt(size_t x, size_t y, const SpreadsheetCell& cell);
+	SpreadsheetCell& getCellAt(size_t x, size_t y);
+
+	void swap(Spreadsheet& other) noexcept;
+
+private:
+	void verifyCoordinate(size_t x, size_t y) const;
+
+	size_t m_width{ 0 };
+	size_t m_height{ 0 };
+	SpreadsheetCell** m_cells{ nullptr };
+};
+
+void swap(Spreadsheet& first, Spreadsheet& second) noexcept;
